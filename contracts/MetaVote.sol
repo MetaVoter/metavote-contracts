a2afeeba0b1f6dcaecccd364e4a6962177a61a81
adamstallard <205792+adamstallard@users.noreply.github.com>
2023-02-27 16:27:45 -0700

    update comments

--- contracts/MetaVote.sol
+++ contracts/MetaVote.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract MetaVote {
     // Storage
+
+    /// A mapping of elections to the addresses of their creators
     mapping (bytes32 => address) public elections;
 
     // Events
@@ -10,7 +12,6 @@ contract MetaVote {
     event Vote(address indexed voter, bytes value, bytes32 indexed electionId);
     event TagAdded(bytes32 indexed electionId, string indexed tag);
 
-    /// @notice
     /// Election metadata should conform to the MetaVote JSON Schema.
     /// @param electionId  A user-generated unique identifier for the election
     /// @param metadataURI  A URI to the metadata describing the election. Immutable data is preferred.
@@ -29,9 +30,8 @@ contract MetaVote {
         emit Vote(msg.sender, value);
     }
 
-    // @notice
-    // Add a tag to an election to allow searching and filtering.
-    // The message sender must be the address that created the election via createElection().
+    /// Add a tag to an election to allow searching and filtering.
+    /// `msg.sender` must be the address that created the election via createElection().
     function addSearchableTag(bytes32 electionId, string tag) external {
         require(elections[electionId] != address(0x0), "Election doesn't exist.");
         require(msg.sender == elections[electionId], "Sender isn't election creator.");
